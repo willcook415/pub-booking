3c32e6b2d2229f5f3c6e9b560cf1b9f536167202
William Cook <williamcook4701@gmail.com>
2025-07-09 10:53:53 +0100

    bug fixes

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // this is okay for development; for production you should validate the cert
+      }
+    }
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
---
+++ models/booking.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    date: DataTypes.DATE,
+    time: DataTypes.STRING,
+    partySize: DataTypes.INTEGER,
+    specialRequests: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
