e5148532f62a942e4e4a17ca031f0a9a1d1b713f
William Cook <williamcook4701@gmail.com>
2025-07-11 16:38:21 +0100

    server bugs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const sgMail = require('@sendgrid/mail');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
